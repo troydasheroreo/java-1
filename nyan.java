d00879cc79da0d55660dd6428aff41870e0b21ad
Troy Miller <troydasheroreo@gmail.com>
2015-09-12 09:53:44 -0400

    save

---
+++ nyan.java
@@ -0,0 +1,10 @@
+import skilstak.c;
+
+public class nyan {
+    public static void main(String[] args){
+      System.out.print(c.clear);
+      while (true) {
+        System.out.print(c.rc() + "Nyan ");
+        }
+    }
+}
