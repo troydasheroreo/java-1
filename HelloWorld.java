8707cf26b79b2b5fec2ea310c80c9e4b9e6616b8
Troy Miller <troydasheroreo@gmail.com>
2015-09-12 08:49:08 -0400

    save

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+import skilstak.c;
+
+public class HelloWorld {
+   
+   public static void main(String[] args){
+      System.out.println(c.cl + c.m + "Hello" + c.y +" World!" + c.x);
+           
+   }
+}
