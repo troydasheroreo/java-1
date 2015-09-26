9c66aaeef8774036f3238ee23607a8fe2927ab1a
Troy Miller <troydasheroreo@gmail.com>
2015-09-26 16:54:53 -0400

    save

---
+++ SkilMon/src/test/TestSkilMon.java
@@ -0,0 +1,16 @@
+package test;
+
+import skilstak.skilmon.SkilMon;
+
+public class TestSkilMon {
+
+    public static void main(String[] args) {
+        
+        SkilMon s = new SkilMon();
+        System.out.println("HP" + s.hp);
+        System.out.println("Attack" + s.attack);
+        System.out.println("Speed" +  s.speed);
+        
+    }
+
+}
