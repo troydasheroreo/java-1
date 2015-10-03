71774ece7e108ff771402fa469fe068f53a9f33d
Troy Miller <troydasheroreo@gmail.com>
2015-10-03 17:16:20 -0400

    save

--- foo.java
+++ foo.java
@@ -5,8 +5,8 @@ import java.io.IOException;
 
 public class foo {
     public static void main(String[] args) throws IOException {
-        Map root = new Gson().fromJson(new FileReader(args[0]), Map.class);
-        System.out.println(root);
+        Map root = new Gson().fromJson(new FileReader("./SkilMon/bulbasaur.json"), Map.class);
+        System.out.println(root.get("hp"));
     }
 }    
 
