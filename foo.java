fbecdd76f10e21181ef19177d207efbf6356d2f8
Troy Miller <troydasheroreo@gmail.com>
2015-10-03 16:59:44 -0400

    save

---
+++ foo.java
@@ -0,0 +1,13 @@
+import java.util.Map;
+import com.google.gson.Gson;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class foo {
+    public static void main(String[] args) throws IOException {
+        Map root = new Gson().fromJson(new FileReader(args[0]), Map.class);
+        System.out.println(root);
+    }
+}    
+
+
